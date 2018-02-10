ce4e6a367a61f039a1b486ae685a401ca04b6748
Daniel Berzin Chua <dnl166@gmail.com>
2018-02-10 20:54:21 +0800

    Scrub credentials.js

---
+++ credentials.js
@@ -0,0 +1,9 @@
+module.exports = {
+	cookieSecret: "cookie secret goes here",
+	awsId: "AWS ID goes here",
+	awsSecret: "AWS Secret goes here",
+	email: {
+		user: "SendGrid username goes here",
+		pass: "Send Grid password goes here"
+	}
+};
